2e0de5b0aa5f7fd504826db0dedf1730585db839
John <john@mindhiveinteractive.com>
2021-10-01 10:25:25 -0400

    add to readme and config

---
+++ README.md
@@ -0,0 +1,9 @@
+follow tutorial to write, deploy, mint NFT
+https://ethereum.org/en/developers/tutorials/how-to-write-and-deploy-an-nft/
+
+verify code on ropsten.etherscan.io
+https://hardhat.org/plugins/nomiclabs-hardhat-etherscan.html
+npx hardhat verify --network ropsten DEPLOYED_CONTRACT_ADDRESS
+
+live NFT visible on ropsten network.
+https://ropsten.etherscan.io/address/0x2E11Fe044e58A2Db7a9894dc3203391dDc8d7db4
--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
-const { API_URL, PRIVATE_KEY } = process.env;
+const { API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 module.exports = {
    solidity: "0.8.0",
    defaultNetwork: "ropsten",
@@ -14,5 +14,8 @@ module.exports = {
          url: API_URL,
          accounts: [`0x${PRIVATE_KEY}`]
       }
+   },
+   etherscan: {
+     apiKey: ETHERSCAN_API_KEY
    },
 }
